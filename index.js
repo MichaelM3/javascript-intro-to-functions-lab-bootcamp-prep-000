f8fbf97b1eb8769eecf633dd2d832bd36ee97166
Michael Muniz <michael.muniz94@gmail.com>
2018-08-22 01:42:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -24,6 +24,6 @@ function sayHiToGrandma(string)
     return "I can\'t hear you!"
   else if (string.toUpperCase() === 'HELLO')
     return "YES INDEED!"
-  else if (string = "I love you, Grandma.")
+  else if ("I love you, Grandma.")
     return "I love you, too."
 }
